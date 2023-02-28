8ad8097dd758067bef8653148eec5920d1fa9cd5
Mateo Bellini <bellinimateo872@gmail.com>
2023-02-28 12:17:51 -0300

    Noveno ejercicio

---
+++ js/9-ejercicio.js
@@ -0,0 +1,16 @@
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+const phrase = prompt("Ingrese una frase");
+let phraseVocal;
+
+for (let i = 0; i < phrase.length; i++) {
+  if (
+    phrase.substr(i, 1) === "a" ||
+    phrase.substr(i, 1) === "e" ||
+    phrase.substr(i, 1) === "i" ||
+    phrase.substr(i, 1) === "o" ||
+    phrase.substr(i, 1) === "u"
+  ) {
+    document.write(`${phrase.substr(i, 1)} - `);
+  }
+}

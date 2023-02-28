cb557dd623f045e31da1474e2253a5b382575815
Mateo Bellini <bellinimateo872@gmail.com>
2023-02-28 12:14:48 -0300

    Quinto ejercicio

---
+++ js/5-ejercicio.js
@@ -0,0 +1,5 @@
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+const firstNumber = +prompt("Ingrese un número");
+const secondNumber = +prompt("Ingrese otro número");
+document.write(firstNumber + secondNumber);
